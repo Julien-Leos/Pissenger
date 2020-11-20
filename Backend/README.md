7a87d078fb2afca9c2b621b9f70138cce3995c23
Leos Julien <osx.leos@gmail.com>
2020-11-20 17:05:04 +0100

    Initialize cloud firebase, hot reload and firebase emulator

---
+++ Backend/README.md
@@ -0,0 +1,11 @@
+# Pissenger Backend
+
+## How to run
+
+### Emulator mode
+
+`cd functions && npm run build-watch`
+
+Open a new terminal
+
+`firebase emulators:start`
---
+++ Backend/functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from 'firebase-functions';
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const makeUppercase = functions.firestore.document('/messages/{documentId}')
+.onCreate((snap, context) => {
+    const original = snap.data().original;
+    console.log('Uppercasing UPDATED', context.params.documentId, original);
+    const uppercase = original.toUpperCase();
+    return snap.ref.set({uppercase}, {merge: true});
+});
