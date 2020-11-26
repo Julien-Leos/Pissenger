bdc01f5674ef0d569cec291b2b3f8ac08334bd76
Leos Julien <osx.leos@gmail.com>
2020-11-26 16:10:39 +0100

    Add Utils functions

--- Backend/functions/src/groups/onGroupCreate.ts
+++ Backend/functions/src/groups/onGroupCreate.ts
@@ -1,18 +1,24 @@
-import * as ad from "firebase-admin";
+import * as fb from "firebase-admin";
 import * as fn from "firebase-functions";
+
 import { User } from "../shared/models/user/user.model";
 
+import { getUserById } from "../shared/utils/utils";
+
 export const onGroupCreate = fn.region("europe-west1").https.onCall(async (data, context) => {
-  if (!context.auth) return new fn.https.HttpsError("unauthenticated", "User unauthenticated.");
+  if (!context.auth) return new fn.https.HttpsError("unauthenticated", "", { error: "User unauthenticated." });
 
   if (data.members.length < 1)
-    return new fn.https.HttpsError("invalid-argument", "A group need at least one member other than the creator.");
+    return new fn.https.HttpsError("invalid-argument", "", {
+      error: "A group need at least one member other than the creator.",
+    });
 
-  const user: User = context.auth.;
+  const user = (await getUserById(context.auth.uid)) as User;
+  if (!user) return new fn.https.HttpsError("internal", "", { error: "Can't access to User data." });
 
-  await ad.firestore().collection("groups").add({
+  await fb.firestore().collection("groups").add({
     name: data.name,
     picture: data.picture,
   });
-  return new fn.https.HttpsError("ok", "New group successfully created.");
+  return new fn.https.HttpsError("ok", "", { error: "New group successfully created." });
 });
--- Backend/functions/src/shared/utils/utils.ts
+++ Backend/functions/src/shared/utils/utils.ts
@@ -1,1 +1,16 @@
-// export const getUserById()
+import * as fb from "firebase-admin";
+
+export const getUserById = (userId: string) => {
+  return fb
+    .firestore()
+    .collection("users")
+    .doc(userId)
+    .get()
+    .then((docRef) => {
+      if (docRef.exists) return docRef.data();
+      else return null;
+    })
+    .catch(() => {
+      return null;
+    });
+};
